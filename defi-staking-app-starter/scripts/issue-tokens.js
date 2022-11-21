18d33e776da70bcb667b9ee286bf044e5db6ecaf
Shubham Agrawal <shubham.agrawal.peopleinteractive.in>
2022-11-21 10:58:13 +0530

    code restructure

--- defi-staking-app-starter/scripts/issue-tokens.js
+++
@@ -1,8 +0,0 @@
-const DecentralBank = artifacts.require('DecentralBank');
-
-module.exports = async function issueRewards(callback) {
-    let decentralBank = await DecentralBank.deployed()
-    await decentralBank.issueTokens()
-    console.log("Tokens have been issued successfully!")
-    callback()
-}
